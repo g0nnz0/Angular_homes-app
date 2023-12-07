5bd86496df7453f34011aa1a65066b13dc871996
g0nnz0 <bonzai_rock@hotmail.com>
2023-12-06 23:06:27 -0300

    Creé HousingLocation Component haciendolo custom con input properties.Use directiva ngFor para iterar en una array y mostrarlo. agregué styles

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IHousingLocation } from '../ihousing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <!--Los [] en src significan que estamos usando Property Binding, el cual le dice a angular
+      que lo que está entre comillas es una property de la class-->
+      <!--{{property.value}} es Angular Interpolation syntax, nos permite mesclar values y expresiones dentro de strings-->
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{housingLocation.name}}</h2>
+      <p class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
+    </section>
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  //Al agregarle @Input() a la property estamos diciendole que va a recibir valores desde un componente padre.
+  //El "!" (Non Null Assertion) le dice al compilador que el valor no será null o undefined
+  @Input() housingLocation!: IHousingLocation;
 }
