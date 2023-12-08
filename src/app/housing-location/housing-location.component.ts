ad5c6d44d8930dd223c913d05f27584174f5473f
g0nnz0 <bonzai_rock@hotmail.com>
2023-12-07 21:08:40 -0300

    Creé details components. creé archivo routes.ts y con el agreé routing a housingLocation y details components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IHousingLocation } from '../ihousing-location';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <!--Los [] en src significan que estamos usando Property Binding, el cual le dice a angular
@@ -14,6 +16,7 @@ import { IHousingLocation } from '../ihousing-location';
       <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
       <h2 class="listing-heading">{{housingLocation.name}}</h2>
       <p class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css'
