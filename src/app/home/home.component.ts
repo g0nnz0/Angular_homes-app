f4a64e73a21ada8e0e2b16f5617297a841a4c58b
g0nnz0 <bonzai_rock@hotmail.com>
2023-12-13 00:52:52 -0300

    Le di funcionalidad a la barra de busqueda aplicando template variables y usandolas en functions para filtrar y mostrar los resultados filtrados. En este punto termina el tutorial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,24 +11,35 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form action="">
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
 
     <section class="results">
-      <app-housing-location *ngFor="let housingLoc of housingLocationList" [housingLocation]="housingLoc"></app-housing-location>
+      <app-housing-location *ngFor="let housingLoc of filteredLocationList" [housingLocation]="housingLoc"></app-housing-location>
     </section>
   `,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
   housingLocationList: IHousingLocation[] = [];
   housingService : HousingService = inject(HousingService);
+  filteredLocationList: IHousingLocation[] = [];
 
   constructor(){
     this.housingService.getAllHousingLocations().then((housingLocationList: IHousingLocation[])=>{
       this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
     });
+  }
+
+  filterResults(text: string) {
+    if(!text) this.filteredLocationList = this.housingLocationList;
+    
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
   }
 }
